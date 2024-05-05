1bcbfdd038db30f078df5f27a6df86388182e636
Aidan Hopper <aidanhop1@gmail.com>
2024-05-05 12:10:55 -0700

    full stack dev shit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="relative bg-gray-100 isolate overflow-hidden py-24 sm:py-32 min-h-screen">
       <div className="grid grid-cols-2 gap-4">
         <h1 className="text-4xl font-bold lg:mx-0 text-center text-gray-900">aidanhopper.dev</h1>
-        Full stack dev
+        <h1 className="text-4xl font-bold lg:mx-0 text-center text-gray-900">FULL STACK DEV SHIT</h1>
       </div>
     </div>
   );
