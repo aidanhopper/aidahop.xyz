05b400b6bd1a82a6beba543b4387521aaacbd0ad
Aidan Hopper <aidanhop1@gmail.com>
2024-05-05 12:02:13 -0700

    fixed class name error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div class="relative bg-gray-100 isolate overflow-hidden py-24 sm:py-32 min-h-screen">
-      <div class="grid grid-cols-2 gap-4">
-        <h1 class="text-4xl font-bold lg:mx-0 text-center text-gray-900">aidanhopper.dev</h1>
+    <div className="relative bg-gray-100 isolate overflow-hidden py-24 sm:py-32 min-h-screen">
+      <div className="grid grid-cols-2 gap-4">
+        <h1 className="text-4xl font-bold lg:mx-0 text-center text-gray-900">aidanhopper.dev</h1>
         Full stack dev
       </div>
     </div>
