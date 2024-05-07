15fac4c28aede4dfe3cfe61ec989873e2fe3c144
Aidan Hopper <aidanhop1@gmail.com>
2024-05-06 17:13:09 -0700

    fix

--- src/app/body.tsx
+++ src/app/body.tsx
@@ -1,7 +1,7 @@
 export default function Body() {
   return (
     <>
-      <div className="relative bg-gray-100 isolate overflow-hidden py-24 sm:py-32 min-h-screen text-green-800">
+      <div className="relative bg-gray-100 isolate overflow-hidden py-24 sm:py-32 min-h-screen text-green-700">
         <h1 className="text-center flex justify-center items-center flex-col opacity-100 text-7xl">
           <span className="italic">who&apos;s</span>
           <div className="font-extrabold">
--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,17 +1,7 @@
 export default function Navbar() {
   return (
     <div className="bg-grey-900 text-green-900">
-      <ul className="list">
-        <li>
-          asdf
-        </li>
-        <li>
-          asdf
-        </li>
-        <li>
-          asdf
-        </li>
-      </ul>
+      aslkdfjasdl;fkjasdfk
     </div>
   );
 }
