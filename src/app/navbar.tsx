9dea2a964247afe3758d41d3655f30ff6dfa0d77
Aidan Hopper <aidanhop1@gmail.com>
2024-05-07 20:27:41 -0700

    navbar nice :)

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,9 +1,12 @@
 export default function Navbar() {
   return (
-    <div className="fixed inset-0 text-right bg-grey-900 text-green-700 font-bold mx-3 p-3">
-      <a className="m-3" href="https://github.com/aidanhopper/" target="_blank" rel="noopener noreferrer">github</a>
-      <a className="m-3" href="https://linkedin.com/in/aidanhopper/" target="_blank" rel="noopener noreferrer">linkedin</a>
-      <a className="m-3" href="/resume.pdf" target="_blank" rel="noopener noreferrer">resume</a>
+    <div>
+      <span className="fixed text-4xl font-extrabold mx-3 p-1"> &lt;/&gt; </span>
+      <span className="fixed inset-0 text-right bg-grey-900 text-green-700 font-bold mx-3 p-3 text-xl">
+        <a className="m-3" href="https://github.com/aidanhopper/" target="_blank" rel="noopener noreferrer">github</a>
+        <a className="m-3" href="https://linkedin.com/in/aidanhopper/" target="_blank" rel="noopener noreferrer">linkedin</a>
+        <a className="m-3" href="/resume.pdf" target="_blank" rel="noopener noreferrer">resume</a>
+      </span>
     </div>
   );
 }
