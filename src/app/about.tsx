680ff7ccaeb9131dd563e5be84122c88c962cb65
Aidan Hopper <aidanhop1@gmail.com>
2024-05-11 17:39:01 -0700

    added project cards

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -1,7 +1,9 @@
+import Card from './card'
+
 const About = () => {
   return (
-      <div className="bg-blue-100 h-screen flex flex-col">
-        <div className="container h-1/2 m-auto flex">
+      <div className="bg-blue-100 flex flex-col py-[200px]">
+        <div className="container m-auto flex">
           <div className="flex flex-col flex-auto m-auto w-[400px] p-10 justify-center">
             <div className="-skew-y-3 drop-shadow-[1px_1px_1px_rgba(0,0,0,0.35)] mb-10 flex-auto font-extrabold  text-6xl">
               about me &nbsp; :O
@@ -10,6 +12,10 @@ const About = () => {
               Im a 21 year old student studying computer science based in Washington.
               I make horrible websites :)
 
+              Im a 21 year old student studying computer science based in Washington.
+              Im a 21 year old student studying computer science based in Washington.
+              I make horrible websites :)
+              I make horrible websites :)
               <br/>
               <br/>
               Contact me at <b>aidanhop1@gmail.com</b>.
@@ -19,12 +25,17 @@ const About = () => {
             HERE IS THE LEFT SIDE CONTENT
           </div>
         </div>
-        <div className="flex-auto h-1/2 container m-auto flex flex-col">
-          <div className="drop-shadow-[1px_1px_1px_rgba(0,0,0,0.35)] text-right flex-none font-extrabold skew-y-3 mb-10 text-6xl">
+        <div className="flex-auto h-1/2 container m-auto flex flex-col mt-[200px]">
+          <div className="drop-shadow-[1px_1px_1px_rgba(0,0,0,0.35)] text-right flex-none font-extrabold skew-y-3 mb-20 text-6xl">
             Projects &nbsp; :) (pls check the github)
           </div>
-          <div className="text-right flex-auto">
-            {/* put project cards here*/}
+          <div className="flex text-right flex-auto flex-wrap justify-center">
+            <Card name={"THIS WEBSITE!"} />
+            <Card name={"asdf"} url={"nixconf"} />
+            <Card name={"asdf"} />
+            <Card name={"asdf"} />
+            <Card name={"asdf"} />
+            <Card name={"asdf"} />
           </div>
         </div>
       </div>
---
+++ src/app/card.tsx
@@ -0,0 +1,28 @@
+const Card = ({ name, url }) => {
+
+
+  const classes = `
+    w-[250px] h-[250px] transition duration-1 hover:-translate-y-2
+    hover:translate-x-2 bg-blue-200 rounded-lg m-3 text-left p-6
+    shadow-[7px_7px_3px_rgba(0,0,0,0.2)] flex flex-col
+  `;
+
+  if (url === undefined)
+    url = "";
+
+  const link = "https://github.com/aidanhopper/" + url;
+
+  return (
+    <a className={classes} href={link} target="_blank">
+      <div className="text-xl font-bold pb-3">
+        {name}
+      </div>
+      <div className="">
+        Here is the descirption of the project.
+      </div>
+    </a>
+  );
+
+}
+
+export default Card;
